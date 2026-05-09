b31cacf3bc122e03da94f5c1394f157b37e36506
kaiserkimguin <kpkabasele@gmail.com>
2026-05-10 00:02:51 +0200

    260509 custom handler added and tested

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func main() {
 	ServeMux := http.NewServeMux()
-	ServeMux.Handle("/", http.FileServer(http.Dir(".")))
+	ServeMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	ServeMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: ServeMux,
