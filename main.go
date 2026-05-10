09d34e7fa9445a155bbc07350ff9cb6f8884dedb
kaiserkimguin <kpkabasele@gmail.com>
2026-05-10 17:32:16 +0200

    260510 handlerHealthz/Reset/Metrics implemented and tested

--- main.go
+++ main.go
@@ -1,21 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
+	cfg := &apiConfig{}
 	ServeMux := http.NewServeMux()
-	ServeMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	ServeMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	ServeMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	ServeMux.HandleFunc("/metrics/", cfg.handlerMetrics)
+	ServeMux.HandleFunc("/reset/", cfg.handlerReset)
+	ServeMux.HandleFunc("/healthz", handlerHealthz)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: ServeMux,
 	}
 	log.Fatal(server.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("request-counter reset"))
 }
